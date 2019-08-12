8a0300031430fbf4ee855781a6e453c8452f215c
JFoley <jfoley@sybrin.co.za>
2019-08-12 20:29:08 +0200

    Initial Commit

---
+++ Subscriber/Program.cs
@@ -0,0 +1,27 @@
+﻿using EasyNetQ;
+using Messages;
+using System;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.Subscribe<TextMessage>("test", HandleTextMessage);
+                Console.WriteLine("Listening for message. Hit <return> to quit.");
+                Console.ReadLine();
+
+            }
+        }
+
+        private static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Got message: {textMessage.Text}");
+            Console.ResetColor();
+        }
+    }
+}
